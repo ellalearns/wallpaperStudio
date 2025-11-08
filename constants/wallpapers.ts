cf32ca3efff4c0e794d02b78ffd999f9913177dc
ellalearns <ellalearnscopy@gmail.com>
2025-11-08 18:56:32 +0100

    add wallpapers

---
+++ constants/wallpapers.ts
@@ -0,0 +1,63 @@
+import { images } from "./images";
+
+
+export const wallpapers = [
+    {
+        "name": "Nature",
+        "description": "Mountains, Forest and Landscapes",
+        "wallpapers": [
+            images.nature1,
+            images.nature2,
+            images.nature3
+        ]
+    },
+    {
+        "name": "Abstract",
+        "description": "Modern Geometric and Artistic designs",
+        "wallpapers": [
+            images.abstract1,
+            images.abstract2,
+            images.abstract3,
+            images.abstract4,
+            images.abstract5
+        ]
+    },
+    {
+        "name": "Urban",
+        "description": "Cities, architecture, and street",
+        "wallpapers": [
+            images.urban1,
+            images.urban2
+        ]
+    },
+    {
+        "name": "Space",
+        "description": "Cosmos, planets, and galaxies",
+        "wallpapers": [
+            images.space1,
+            images.space2,
+            images.space3
+        ]
+    },
+    {
+        "name": "Minimalist",
+        "description": "Clean, simple, and elegant",
+        "wallpapers": [
+            images.min1,
+            images.min2,
+            images.min3, 
+            images.min4,
+            images.min5
+        ]
+    },
+    {
+        "name": "Animals",
+        "description": "Wildlife and nature photography",
+        "wallpapers": [
+            images.animals1,
+            images.animals2,
+            images.animals3,
+            images.animals4
+        ]
+    }
+]
